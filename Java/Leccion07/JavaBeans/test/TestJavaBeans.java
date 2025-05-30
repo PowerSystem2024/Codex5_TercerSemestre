8dac408ed6bcd6aebde667243b456770e4d70297
Santino Salatino <salatinosantino1@gmail.com>
2025-05-30 17:19:54 -0300

    Agrego Leccion07 y Leccion08 de Java clase 8 28/05

---
+++ Java/Leccion07/JavaBeans/test/TestJavaBeans.java
@@ -0,0 +1,16 @@
+package JavaBeans.test;
+
+import JavaBeans.domain.Persona;
+
+public class TestJavaBeans {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.setNombre("Juan");
+        persona.setApellido("Perez");
+        System.out.println("Persona = "+persona);
+
+        System.out.println("Persona nombre: "+persona.getNombre());
+        System.out.println("Persona apellido: "+persona.getApellido());
+        
+    }
+}
